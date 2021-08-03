875498cec03eb7636559812aa262f6aa418af92a
AugustoSavi <guto_savi@outlook.com>
2021-08-03 20:16:13 -0300

    ajuste

--- src/main/java/com/binno/dominio/module/usuarioacesso/api/dto/UsuarioAcessoDto.java
+++ src/main/java/com/binno/dominio/module/usuarioacesso/api/dto/UsuarioAcessoDto.java
@@ -1,6 +1,7 @@
 package com.binno.dominio.module.usuarioacesso.api.dto;
 
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
 import javax.validation.constraints.NotNull;
 
@@ -11,15 +12,19 @@ import javax.validation.constraints.NotNull;
 @Builder
 public class UsuarioAcessoDto {
 
-    @NotNull(message = "login obrigatorio")
+    @NotNull(message = "Login obrigatório")
+    @Length(min = 4, max = 200, message = "o comprimento do login deve ser entre 5 e 200")
     private String login;
 
     private Integer tenant;
 
-    @NotNull(message = "senha obrigatorio")
+    @NotNull(message = "Senha obrigatório")
+    @Length(min = 4, max = 200, message = "o comprimento da senha deve ser entre 5 e 200")
     private String password;
 
     private String nome;
 
+    @NotNull(message = "E-mail obrigatório")
+    @Length(min = 5, max = 200, message = "o comprimento do e-mail deve ser entre 5 e 200")
     private String email;
 }
