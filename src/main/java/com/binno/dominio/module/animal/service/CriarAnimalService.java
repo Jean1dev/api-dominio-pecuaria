a126b9ffd1a5e17dfaf0f29d736370e1efb3e316
AugustoSavi <guto_savi@outlook.com>
2021-08-24 22:03:32 -0300

    change request closes

--- src/main/java/com/binno/dominio/module/animal/service/CriarAnimalService.java
+++ src/main/java/com/binno/dominio/module/animal/service/CriarAnimalService.java
@@ -77,14 +77,12 @@ public class CriarAnimalService implements RegraNegocioService<Animal, CriarAnim
         criarAnimalDto
                 .getPesos()
                 .forEach(peso -> {
-                    if(peso.getPeso() > 0) {
                         pesoAnimalRepository.save(PesoAnimal.builder()
                                 .id(peso.getId())
                                 .dataPesagem(peso.getDataPesagem())
                                 .peso(peso.getPeso())
                                 .animal(animal)
                                 .build());
-                    }
                 });
     }
 }
