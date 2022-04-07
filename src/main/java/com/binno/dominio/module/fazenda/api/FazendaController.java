bd9ba874c32461f714ab8b773e1faf0d0ec02e67
Jean <jeanlucafp@gmail.com>
2022-04-07 17:37:46 -0300

    feature balanceamento

--- src/main/java/com/binno/dominio/module/fazenda/api/FazendaController.java
+++ src/main/java/com/binno/dominio/module/fazenda/api/FazendaController.java
@@ -7,6 +7,7 @@ import com.binno.dominio.module.fazenda.api.dto.QuantidadeAnimalPorFazendaDto;
 import com.binno.dominio.module.fazenda.model.Fazenda;
 import com.binno.dominio.module.fazenda.repository.FazendaRepository;
 import com.binno.dominio.module.fazenda.service.QuantidadeAnimaisPorFazendaService;
+import com.binno.dominio.module.fazenda.service.RebalanceamentoAnimaisNasFazendasService;
 import com.binno.dominio.module.tenant.model.Tenant;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.domain.Specification;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -34,10 +36,9 @@ public class FazendaController {
     public static final String PATH = "fazendas";
 
     private final FazendaRepository repository;
-
     private final AuthenticationHolder holder;
-
     private final QuantidadeAnimaisPorFazendaService quantidadeAnimaisPorFazendaService;
+    private final RebalanceamentoAnimaisNasFazendasService rebalanceamentoAnimaisNasFazendasService;
 
     @GetMapping(path = "total-animais-por-fazenda")
     public List<QuantidadeAnimalPorFazendaDto> totalAnimaisPorFazenda() {
@@ -60,6 +61,12 @@ public class FazendaController {
             @RequestParam(value = "page", defaultValue = "0", required = false) int page,
             @RequestParam(value = "size", defaultValue = "10", required = false) int size) {
         return pageToDto(repository.findAll(Specification.where(tenant(holder.getTenantId()).and(nome(nome))), PageRequest.of(page, size)));
+    }
+
+    @PostMapping(path = "/balancear")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void balancear() {
+        rebalanceamentoAnimaisNasFazendasService.balancear();
     }
 
     @PostMapping
---
+++ src/main/java/com/binno/dominio/module/fazenda/service/RebalanceamentoAnimaisNasFazendasService.java
@@ -0,0 +1,97 @@
+package com.binno.dominio.module.fazenda.service;
+
+import com.binno.dominio.context.AuthenticationHolder;
+import com.binno.dominio.module.animal.model.Animal;
+import com.binno.dominio.module.animal.repository.AnimalRepository;
+import com.binno.dominio.module.fazenda.model.Fazenda;
+import com.binno.dominio.module.fazenda.repository.FazendaRepository;
+import com.binno.dominio.module.notificacao.service.RegistrarNotificacao;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+@EnableAsync
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class RebalanceamentoAnimaisNasFazendasService {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RebalanceamentoAnimaisNasFazendasService.class);
+
+    private final FazendaRepository fazendaRepository;
+    private final AnimalRepository animalRepository;
+    private final RegistrarNotificacao registrarNotificacao;
+    private final AuthenticationHolder holder;
+
+    /*
+     * @Docs
+     * objetivo desse metodo é deixar todas as fazendas com animais que atendam a sua capacidade maxima
+     * */
+    @Async
+    public void balancear() {
+        List<Fazenda> fazendas = fazendaRepository.findAllByTenantId(holder.getTenantId());
+        List<Fazenda> fazendasComCapacidadesEstourada = fazendas.stream()
+                .filter(fazenda -> fazenda.getAnimais().size() > fazenda.getCapMaximaGado())
+                .collect(Collectors.toList());
+
+        if (fazendasComCapacidadesEstourada.isEmpty()) {
+            registrarNotificacao.executar("Não tem necessidade de fazer balanceamento nas fazendas");
+            return;
+        }
+
+        List<Fazenda> fazendasDisponiveis = fazendas.stream()
+                .filter(fazenda -> fazenda.getCapMaximaGado() > fazenda.getAnimais().size())
+                .collect(Collectors.toList());
+
+        if (fazendasDisponiveis.isEmpty()) {
+            registrarNotificacao.executar("Não existem fazendas disponveis para o balanceamento");
+            return;
+        }
+
+        fazendasComCapacidadesEstourada.forEach(fazenda -> {
+            int quantidadeAnimaisExcedentes = fazenda.getAnimais().size() - fazenda.getCapMaximaGado();
+            levarAnimaisParaOutraFazenda(quantidadeAnimaisExcedentes, fazenda, fazendasDisponiveis);
+        });
+
+        registrarNotificacao.executar("Balanceamento finalizado");
+    }
+
+    private void levarAnimaisParaOutraFazenda(int quantidadeAnimaisExcedentes, Fazenda fazenda, List<Fazenda> fazendasDisponiveis) {
+        fazendasDisponiveis.stream()
+                .filter(f1 -> !f1.equals(fazenda))
+                .filter(f1 -> !f1.getBalanceada())
+                .collect(Collectors.toList())
+                .stream()
+                .findFirst()
+                .ifPresent(fazendaASerBalanceada -> {
+                    int quantidadePossivelInserir = fazendaASerBalanceada.getCapMaximaGado() - fazendaASerBalanceada.getAnimais().size();
+                    List<Animal> animais = new ArrayList<>();
+                    for (int i = 0; i < quantidadeAnimaisExcedentes; i++) {
+                        Animal animal = fazenda.getAnimais().get(i);
+
+                        if (animais.size() < quantidadePossivelInserir) {
+                            LOGGER.info(animal.getApelido() + " saiu da fazenda " + animal.getFazenda().getNome() + " para " + fazendaASerBalanceada.getNome());
+                            animal.setFazenda(fazendaASerBalanceada);
+                            animais.add(animal);
+                        }
+                    }
+
+                    fazendaASerBalanceada.setBalanceada(true);
+                    LOGGER.info(fazendaASerBalanceada.getNome() + " balanceado");
+
+                    if (!animais.isEmpty()) {
+                        animalRepository.saveAll(animais);
+                    }
+
+                });
+    }
+}
