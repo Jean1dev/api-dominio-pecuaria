71f9ed038284b73819f922d31456b87521ba2c6b
Jeanluca <jeanlucafp@gmail.com>
2022-10-24 10:06:09 -0300

    Update ApplicationConfigIT.java
    solution: https://github.com/spring-projects/spring-boot/issues/19788

--- src/test/java/com/binno/dominio/ApplicationConfigIT.java
+++ src/test/java/com/binno/dominio/ApplicationConfigIT.java
@@ -2,10 +2,8 @@ package com.binno.dominio;
 
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
-@ActiveProfiles(profiles = "test")
+@SpringBootTest(properties = "spring.profiles.active:test")
 @AutoConfigureMockMvc
 public abstract class ApplicationConfigIT {
 }
